e57bc14467a7e91e1c9d2b4996af865f09feb36c
Jorge Rivera <jorge117rivera@gnail.com>
2022-02-25 15:50:38 -0600

    retocando layout

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, url_for, redirect
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -14,13 +14,15 @@ def index():
 
 @app.route('/about/')
 def estudios():
-    estudios = ['Tecnico en Redes','Tecnico en Reparacion de Computadoras','Tecnico en Reparacion de Celulares','Ciencia de Computadoras - CS50',
-                'Estudiante Ingeniria en Computacion','Graduado Br Escuela Normal Maria Mazzarello','Academia Sabatina Jovenes Talento']
-    data={
+    estudios = ['Tecnico en Redes','Tecnico en Reparacion de Computadoras','Tecnico en Reparacion de Celulares',
+                'Ciencia de Computadoras - CS50','Estudiante Ingeniria en Computacion',
+                'Graduado Br Escuela Normal Maria Mazzarello','Academia Sabatina Jovenes Tlento']
+    data={ 
         'estudios': estudios} 
     return render_template('about.html',data=data)
 
 def noencontrado(error):
+    print('404')
     return render_template("404.html")
      
 if __name__ == '__main__':
